a2768b8d6295a88c1bedb9754e73f517a7d32820
Abhisek Panigrahi <pnabhisek@gmail.com>
2018-04-23 07:22:52 +0530

    Create one hot encoding for output labels

--- preprocess.py
+++ preprocess.py
@@ -2,6 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from mnist import MNIST
+import numpy as np
+
+def create_one_hot(array):
+    '''
+    Create one hot encoding
+    '''
+
+    array_length = len(array)
+    array = np.array(array)
+    array_final = np.zeros((array_length, 10))
+    array_final[np.arange(array_length), array] = 1
+
+    return array_final
 
 def read_image(directory):
     '''
@@ -13,6 +26,13 @@ def read_image(directory):
 
     train_images, train_labels = mndata.load_training()
     test_images, test_labels = mndata.load_testing()
+
+    num_training = len(train_images)
+    num_testing = len(test_images)
+
+    # Create one hot encoding
+    train_labels = create_one_hot(train_labels)
+    test_labels = create_one_hot(test_labels)
 
     return (train_images, train_labels,
             test_images, test_labels)
